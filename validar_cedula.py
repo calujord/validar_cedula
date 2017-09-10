1673e55c585361a193a2c8dcbc628409b2ae412a
Carlos Luis Jordán <carlos.jordan.murillo@est.fib.upc.edu>
2017-09-10 12:09:29 +0200

    Add files via upload
    validar número de cédulas de Ecuador en python

---
+++ validar_cedula.py
@@ -0,0 +1,25 @@
+import math
+
+
+def verificar_cedula(cedula=""):
+    if len(cedula) != 10:
+        raise Exception("Error numero de cedula incompleto")
+    else:
+        multiplicador = [2, 1, 2, 1, 2, 1, 2, 1, 2]
+        ced_array = map(lambda k: int(k), list(cedula))[0:9]
+        ultimo_digito = int(cedula[9])
+        resultado = []
+        arr = map(lambda x, j: (x, j), ced_array, multiplicador)
+        for (i, j) in arr:
+            if i * j < 10:
+                resultado.append(i * j)
+            else:
+                resultado.append((i * j)-9)
+        print resultado
+        if ultimo_digito == int(math.ceil(float(sum(resultado)) / 10) * 10) - sum(resultado):
+            return True
+        else:
+            return False
+
+
+print verificar_cedula("0923797856")
